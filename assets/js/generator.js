c3387e3f5d32175b1742a039f603c0af7b5fe864
Nougator <37145431+nougator@users.noreply.github.com>
2020-06-20 22:23:58 +0200

    Initial commit

---
+++ assets/js/generator.js
@@ -0,0 +1,34 @@
+let elm = document.getElementById('display-username');
+
+let soundingChars = ['a', 'i', 'e', 'y', 'o', 'u'];
+let nonSoundingChars = ['q', 'w', 'b', 'r', 't', 'z', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'x', 'c', 'v', 'b', 'n', 'm'];
+let name = '';
+let charType = true;
+
+generateUsername();
+
+function generateUsername(){
+    let nameSize = Math.floor(Math.random() * 5) + 4;
+    let currentNameStep = 0;
+    while(currentNameStep < nameSize){
+        if(getCharType()){
+            if(currentNameStep == 0) name = name + soundingChars[Math.floor(Math.random() * soundingChars.length)].toUpperCase();
+            else name = name + nonSoundingChars[Math.floor(Math.random() * soundingChars.length)];
+        }else{
+            if(currentNameStep == 0) name = name + soundingChars[Math.floor(Math.random() * soundingChars.length)].toUpperCase();
+            else name = name + soundingChars[Math.floor(Math.random() * soundingChars.length)];
+        }
+        currentNameStep++;
+    }
+    elm.innerHTML = name;
+    name = '';
+}
+function getCharType(){
+    if(charType){
+        charType = false;
+        return charType;
+    }else{
+        charType = true;
+        return charType;
+    }
+}
